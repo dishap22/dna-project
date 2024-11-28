f2d6779c494f1dca1e9a370a4c6878f04e72601b
Jakka Krishna Jatin <156399869+KJsquare9@users.noreply.github.com>
2024-11-28 13:00:05 +0530

    Update krishna.py

--- krishna.py
+++ krishna.py
@@ -4,9 +4,8 @@ import pymysql.cursors
 
 # ============== [Functional Requirement B] ==============
 
-def insertNewUser(cur, con, name, email, password, is_premium, profile_picture):
-    # Check that none of the fields are empty, if they are, return None and print an error message
-    if not all([name, email, password, is_premium]):
+def insertNewUser(name, email, password, is_premium, profile_picture):
+    if name == "" or email == "" or password == "" or is_premium == "":
         print("Error: One or more fields are empty")
         return None
 
@@ -15,93 +14,133 @@ def insertNewUser(cur, con, name, email, password, is_premium, profile_picture):
         cur.execute(query, (name, email, password, is_premium, profile_picture))
         con.commit()
         print("User inserted successfully")
-
-        # Obtain and return the user_id of the newly inserted user
-        query = "SELECT User_ID FROM User WHERE Email = %s"
-        cur.execute(query, (email,))
-        user_id = cur.fetchone()['User_ID']
-        return user_id
-
     except Exception as e:
         con.rollback()
         print("Failed to insert user")
         print(">>>>>>>>>>>>>", e)
         return None
 
-
-def insertPremiumUser(cur, con, user_id, plan, billing_date, amount):
+    # Obtain and return the user_id of the newly inserted user
     try:
-        query = "INSERT INTO PremiumUsers (User_ID, Plan, Billing_Date, Amount_to_Be_Paid) VALUES (%s, %s, %s, %s)"
-        cur.execute(query, (user_id, plan, billing_date, amount))
-        con.commit()
-        print("Premium user inserted successfully")
+        query = "SELECT User_ID FROM User WHERE Email = %s"
+        cur.execute(query, (email,))
+        user_id = cur.fetchone()['User_ID']
+        print("User ID obtained successfully")
+        return user_id
     except Exception as e:
         con.rollback()
-        print("Failed to insert premium user")
+        print("Failed to fetch user ID")
+        print(">>>>>>>>>>>>>", e)
+        return None
+        
+
+def insertPremiumUser(user_id, is_premium, plan, billing_date, amount):
+
+    if is_premium:
+        try:
+            query = "INSERT INTO PremiumUsers (User_ID, Plan, Billing_Date, Amount_to_Be_Paid) VALUES (%s, %s, %s, %s)"
+            cur.execute(query, (user_id, plan, billing_date, amount))
+            con.commit()
+
+            print("Premium user inserted successfully")
+
+        except Exception as e:
+            con.rollback()
+            print("Failed to insert premium user")
+            print(">>>>>>>>>>>>>", e)
+
+
+def updateProfilePicture(user_id, profile_picture):
+    
+    # if the user exists, update their profile picture
+
+    try:
+        query = "UPDATE User SET Profile_Image = %s WHERE User_ID = %s"
+        cur.execute(query, (profile_picture, user_id))
+        con.commit()
+
+        print("Profile picture updated successfully")
+
+    except Exception as e:
+        con.rollback()
+        print("Failed to update profile picture")
         print(">>>>>>>>>>>>>", e)
 
 
-def removeTrackLike(cur, con, user_id, track_id):
+def removeUser(user_id):
+    
+    # if the user exists, remove them from the database
     try:
-        query = "DELETE FROM TrackLikes WHERE User_ID = %s AND Track_ID = %s"
-        cur.execute(query, (user_id, track_id))
-        con.commit()
-        print("Track like removed successfully")
-    except Exception as e:
-        con.rollback()
-        print("Failed to remove track like")
-        print(">>>>>>>>>>>>>", e)
-
-
-def followArtist(cur, con, user_id, artist_id):
-    try:
-        query = "INSERT INTO ArtistFollowers (User_ID, Artist_ID) VALUES (%s, %s)"
-        cur.execute(query, (user_id, artist_id))
-        con.commit()
-        print("Artist followed successfully")
-    except Exception as e:
-        con.rollback()
-        print("Failed to follow artist")
-        print(">>>>>>>>>>>>>", e)
-
-
-def unfollowArtist(cur, con, user_id, artist_id):
-    try:
-        query = "DELETE FROM ArtistFollowers WHERE User_ID = %s AND Artist_ID = %s"
-        cur.execute(query, (user_id, artist_id))
-        con.commit()
-        print("Artist unfollowed successfully")
-    except Exception as e:
-        con.rollback()
-        print("Failed to unfollow artist")
-        print(">>>>>>>>>>>>>", e)
-
-
-def subscribeToPremium(cur, con, user_id, plan, billing_date, amount):
-    try:
-        query = "INSERT INTO PremiumUsers (User_ID, Plan, Billing_Date, Amount_to_Be_Paid) VALUES (%s, %s, %s, %s)"
-        cur.execute(query, (user_id, plan, billing_date, amount))
-        con.commit()
-        print("Subscribed to premium successfully")
-    except Exception as e:
-        con.rollback()
-        print("Failed to subscribe to premium")
-        print(">>>>>>>>>>>>>", e)
-
-
-def unsubscribeToPremium(cur, con, user_id):
-    try:
-        query = "DELETE FROM PremiumUsers WHERE User_ID = %s"
+        query = "DELETE FROM User WHERE User_ID = %s"
         cur.execute(query, (user_id,))
         con.commit()
-        print("Unsubscribed from premium successfully")
+
+        print("User removed successfully")
+
     except Exception as e:
         con.rollback()
-        print("Failed to unsubscribe from premium")
+        print("Failed to remove user")
         print(">>>>>>>>>>>>>", e)
 
 
-def dispatch(ch, cur, con):
+def removeUserPlaylist(user_id, playlist_id):
+    
+    
+    # if the user and playlist exist, remove the playlist from the user's playlists
+    try:
+        query = "DELETE FROM UserOwnsPlaylist WHERE User_ID = %s AND Playlist_ID = %s"
+        cur.execute(query, (user_id, playlist_id))
+        con.commit()
+
+        print("Playlist removed successfully")
+
+    except Exception as e:
+        con.rollback()
+        print("Failed to remove playlist")
+        print(">>>>>>>>>>>>>", e)
+
+
+
+# ============== [Functional Requirement G] ==============
+
+def createNewAlbum(name, release_date, artist_id, genre, cover_art):
+
+    # if the artist exists, insert the new album
+
+    try:
+        query = "INSERT INTO Album (Name, Release_Date, Artist_ID, Genre, Cover_Art) VALUES (%s, %s, %s, %s, %s)"
+        cur.execute(query, (name, release_date, artist_id, genre, cover_art))
+        con.commit()
+
+        print("Album inserted successfully")
+
+    except Exception as e:
+        con.rollback()
+        print("Failed to insert album")
+        print(">>>>>>>>>>>>>", e)
+
+
+def updateAlbumDetails(album_id, name, release_date, genre, cover_art):
+    
+
+        # if the album exists, update its details
+    
+        try:
+            query = "UPDATE Album SET Name = %s, Release_Date = %s, Genre = %s, Cover_Art = %s WHERE Album_ID = %s"
+            cur.execute(query, (name, release_date, genre, cover_art, album_id))
+            con.commit()
+    
+            print("Album details updated successfully")
+    
+        except Exception as e:
+            con.rollback()
+            print("Failed to update album details")
+            print(">>>>>>>>>>>>>", e)
+
+
+############################################################################################################
+
+def dispatch(ch):
     """
     Function that maps helper functions to option entered
     """
@@ -109,85 +148,113 @@ def dispatch(ch, cur, con):
         name = input("Enter Name: ")
         email = input("Enter Email: ")
         password = input("Enter Password: ")
-        is_premium = int(input("Is Premium (1 for Yes, 0 for No): "))
-        profile_picture = input("Enter Profile Picture URL: ")
+        is_premium = input("Is Premium (1 for Yes, 0 for No): ")
+        profile_picture = input("Enter Profile Picture Path: ")
 
-        user_id = insertNewUser(cur, con, name, email, password, is_premium, profile_picture)
-        if user_id and is_premium:
+        try:
+            is_premium = int(is_premium)
+            if is_premium not in [0, 1]:
+                raise ValueError("Is Premium must be 0 or 1.")
+        except ValueError:
+            print("Error: Invalid value for Is Premium. Must be 0 or 1.")
+            return
+
+        user_id = insertNewUser(name, email, password, is_premium, profile_picture)
+        if user_id is None:
+            return
+
+        if is_premium == 1:
             plan = input("Enter Plan: ")
             billing_date = input("Enter Billing Date (YYYY-MM-DD): ")
-            amount = float(input("Enter Amount: "))
-            insertPremiumUser(cur, con, user_id, plan, billing_date, amount)
+            amount = input("Enter Amount to be Paid: ")
+            try:
+                amount = float(amount)
+            except ValueError:
+                print("Error: Invalid amount.")
+                return
+            insertPremiumUser(user_id, is_premium, plan, billing_date, amount)
 
     elif ch == 2:
         user_id = input("Enter User ID: ")
-        track_id = input("Enter Track ID: ")
-        removeTrackLike(cur, con, user_id, track_id)
+        profile_picture = input("Enter New Profile Picture Path: ")
+        updateProfilePicture(user_id, profile_picture)
 
     elif ch == 3:
         user_id = input("Enter User ID: ")
-        artist_id = input("Enter Artist ID: ")
-        followArtist(cur, con, user_id, artist_id)
+        removeUser(user_id)
 
     elif ch == 4:
         user_id = input("Enter User ID: ")
-        artist_id = input("Enter Artist ID: ")
-        unfollowArtist(cur, con, user_id, artist_id)
+        playlist_id = input("Enter Playlist ID: ")
+        removeUserPlaylist(user_id, playlist_id)
 
     elif ch == 5:
-        user_id = input("Enter User ID: ")
-        plan = input("Enter Plan: ")
-        billing_date = input("Enter Billing Date (YYYY-MM-DD): ")
-        amount = float(input("Enter Amount: "))
-        subscribeToPremium(cur, con, user_id, plan, billing_date, amount)
+        name = input("Enter Album Name: ")
+        release_date = input("Enter Release Date (YYYY-MM-DD): ")
+        artist_id = input("Enter Artist ID: ")
+        genre = input("Enter Genre: ")
+        cover_art = input("Enter Cover Art Path: ")
+        createNewAlbum(name, release_date, artist_id, genre, cover_art)
 
     elif ch == 6:
-        user_id = input("Enter User ID: ")
-        unsubscribeToPremium(cur, con, user_id)
+        album_id = input("Enter Album ID: ")
+        name = input("Enter New Album Name: ")
+        release_date = input("Enter New Release Date (YYYY-MM-DD): ")
+        genre = input("Enter New Genre: ")
+        cover_art = input("Enter New Cover Art Path: ")
+        updateAlbumDetails(album_id, name, release_date, genre, cover_art)
 
     else:
         print("Error: Invalid Option")
 
 
-# Main loop
-while True:
-    sp.call('clear', shell=True)
+# Global
+while(1):
+    tmp = sp.call('clear', shell=True)
+    
+    # Can be skipped if you want to hardcode username and password
+    username = input("Username: ")
+    password = input("Password: ")
 
     try:
+        # Set db name accordingly which have been create by you
+        # Set host to the server's address if you don't want to use local SQL server 
         con = pymysql.connect(host='localhost',
                               port=30306,
                               user="root",
                               password="password",
                               db='COMPANY',
                               cursorclass=pymysql.cursors.DictCursor)
+        tmp = sp.call('clear', shell=True)
 
-        if con.open:
+        if(con.open):
             print("Connected")
         else:
             print("Failed to connect")
 
-        input("Enter any key to CONTINUE>")
+        tmp = input("Enter any key to CONTINUE>")
 
         with con.cursor() as cur:
-            while True:
-                sp.call('clear', shell=True)
-                print("1. Add new user")
-                print("2. Unlike a track")
-                print("3. Follow an artist")
-                print("4. Unfollow an artist")
-                print("5. Subscribe to premium")
-                print("6. Unsubscribe to premium")
+            while(1):
+                tmp = sp.call('clear', shell=True)
+                # Here taking example of Employee Mini-world
+                print("1. Insert a new user")
+                print("2. Update a user's profile picture")
+                print("3. Remove a user")
+                print("4. Remove a playlist from a user")
+                print("5. Create a new album")
+                print("6. Update album details")
                 print("7. Logout")
                 ch = int(input("Enter choice> "))
-                sp.call('clear', shell=True)
+                tmp = sp.call('clear', shell=True)
                 if ch == 7:
                     exit()
                 else:
-                    dispatch(ch, cur, con)
-                    input("Enter any key to CONTINUE>")
+                    dispatch(ch)
+                    tmp = input("Enter any key to CONTINUE>")
 
     except Exception as e:
-        sp.call('clear', shell=True)
+        tmp = sp.call('clear', shell=True)
         print(e)
-        print("Connection Refused: Check credentials or DB access")
-        input("Enter any key to CONTINUE>")
+        print("Connection Refused: Either username or password is incorrect or user doesn't have access to database")
+        tmp = input("Enter any key to CONTINUE>")
